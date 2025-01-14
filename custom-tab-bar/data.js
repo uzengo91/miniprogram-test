370752bafe11d8ffa3959b912304004f4f616800
luhao <luhao@xiaobing.ai>
2025-01-14 23:43:38 +0800

    初始化版本

---
+++ custom-tab-bar/data.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    icon: 'home',
+    text: '首页',
+    url: 'pages/home/home',
+  },
+  {
+    icon: 'sort',
+    text: '分类',
+    url: 'pages/goods/category/index',
+  }
+];
